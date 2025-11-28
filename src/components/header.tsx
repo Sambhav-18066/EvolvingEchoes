b8cddbf738bf39366679736f564bdda8badc8891
Creator two <creator2@cutmap.ac.in>
2025-11-28 18:04:54 +0000

    23:33:37.081  ⨯ Next.js build worker exited with code: 1 and signal: nul

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,6 +17,8 @@ import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { useUser, useDoc, useMemoFirebase } from '@/firebase';
 import { getFirestore, doc } from 'firebase/firestore';
 import { Skeleton } from './ui/skeleton';
+import { signOut, getAuth } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 type HeaderProps = {
   isLoggedIn?: boolean;
@@ -26,6 +28,8 @@ export function Header({ isLoggedIn = false }: HeaderProps) {
   const userAvatar = PlaceHolderImages.find(p => p.id === 'user-avatar-1');
   const { user, isUserLoading } = useUser();
   const db = getFirestore();
+  const auth = getAuth();
+  const router = useRouter();
 
   const userProfileRef = useMemoFirebase(() => {
     if (!user) return null;
@@ -33,6 +37,11 @@ export function Header({ isLoggedIn = false }: HeaderProps) {
   }, [db, user]);
 
   const { data: userProfile, isLoading: isProfileLoading } = useDoc(userProfileRef);
+
+  const handleSignOut = async () => {
+    await signOut(auth);
+    router.push('/');
+  }
 
   const isLoading = isUserLoading || isProfileLoading;
   
@@ -66,7 +75,7 @@ export function Header({ isLoggedIn = false }: HeaderProps) {
                   <DropdownMenuItem asChild><Link href="#">Profile</Link></DropdownMenuItem>
                   <DropdownMenuItem asChild><Link href="/dashboard">Dashboard</Link></DropdownMenuItem>
                   <DropdownMenuSeparator />
-                  <DropdownMenuItem asChild><Link href="/">Logout</Link></DropdownMenuItem>
+                  <DropdownMenuItem onClick={handleSignOut}>Logout</DropdownMenuItem>
                 </DropdownMenuContent>
               </DropdownMenu>
             </>
